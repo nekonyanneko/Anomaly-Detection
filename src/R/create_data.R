dd91aec0809fbb8d5d115dcc34cee16980305ecc
nekonyanneko <morita.d.shun@gmail.com>
2017-05-28 03:33:36 +0900

    fasttextで教師あり学習の分類

--- src/R/create_data.R
+++ src/R/create_data.R
@@ -1,15 +1,15 @@
 library(dplyr)
 
-create_data_num <- 1000
-time_range <- 300
-anomaly_num <- 10
+create_data_num <- 10000
+time_range <- 100
+anomaly_num <- 100
 period_num <- 6
 
 # Creat Data
-white_noize <- rnorm(time_range)
+white_noize <- as.integer(rnorm(time_range)*10)
 df = data.frame(white_noize)
 for(i in 2:create_data_num){
-  white_noize <- rnorm(time_range)
+  white_noize <- as.integer(rnorm(time_range)*10)
   #period <-rep(c(15,rep(0,time_range/period_num-1)),period_num)
   #df[,i] <- white_noize + period[0:300]
   df[,i] <- white_noize
@@ -17,7 +17,7 @@ for(i in 2:create_data_num){
 }
 
 # Anomaly Data
-Anomaly <- runif(anomaly_num, min = 5, max = 10)
+Anomaly <- as.integer(runif(anomaly_num, min = 60, max = 100))
 target_index <- as.integer(runif(anomaly_num, min = 1, max = time_range))
 for(i in 1:anomaly_num){
   df[target_index[i],i] <- Anomaly[i]
@@ -27,9 +27,10 @@ for(i in 1:anomaly_num){
 # OutPut
 #out <- file("./data.txt", "w")
 out <- file("./data_ft.txt", "w")
+#out <- file("./data_ft_test.txt", "w")
 for(i in 1:create_data_num) {
-  if(i <= anomaly_num) writeLines(text = sprintf("__label__1"), con = out, sep=",")
-  else writeLines(text = sprintf("__label__2"), con = out, sep=",")
+  if(i <= anomaly_num) writeLines(text = sprintf("__label__1"), con = out, sep=" ")
+  else writeLines(text = sprintf("__label__2"), con = out, sep=" ")
   for(j in 1:time_range){
     if(j == time_range) writeLines(text = sprintf(toString(df[,i][j])), con = out, sep="\n")
     #else writeLines(text = sprintf(toString(df[,i][j])), out, sep=",")
