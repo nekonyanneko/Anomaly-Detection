6dc68545c9bf1f889841791f76ad71d787e38c69
nekonyanneko <morita.d.shun@gmail.com>
2017-05-27 23:48:52 +0900

    Add code.R to create Sample Data

---
+++ src/R/create_data.R
@@ -0,0 +1,35 @@
+library(dplyr)
+
+create_data_num <- 1000
+time_range <- 300
+anomaly_num <- 100
+period_num <- 6
+
+# Creat Data
+white_noize <- rnorm(time_range)
+df = data.frame(white_noize)
+for(i in 2:create_data_num){
+  white_noize <- rnorm(time_range)
+  #period <-rep(c(15,rep(0,time_range/period_num-1)),period_num)
+  #df[,i] <- white_noize + period[0:300]
+  df[,i] <- white_noize
+  #plot(df[,i],type="l")
+}
+
+# Anomaly Data
+Anomaly <- runif(anomaly_num, min = 5, max = 10)
+target_index <- as.integer(runif(anomaly_num, min = 1, max = time_range))
+for(i in 1:anomaly_num){
+  df[target_index[i],i] <- Anomaly[i]
+  #plot(df[,i],type="l")
+}
+
+# OutPut
+out <- file("./data.txt", "w")
+for(i in 1:create_data_num) {
+  for(j in 1:time_range){
+    if(j == time_range) writeLines(text = sprintf(toString(df[,i][j])), con = out, sep="\n")
+    else writeLines(text = sprintf(toString(df[,i][j])), out, sep=",")
+  }
+}
+close(out)
