34e1fb223f71247f12169c85a872d30fa395d925
nekonyanneko <morita.d.shun@gmail.com>
2017-05-28 23:40:31 +0900

    lstmでAnomaly detection

---
+++ src/keras/lstm.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+import numpy as np
+from numpy import *
+from sklearn.preprocessing import MinMaxScaler
+
+length_of_sequences = 50 # 入力データの次元
+in_out_neurons = 50 # 出力データの次元
+TRAIN_PATH = './../R/data_lstm.txt'
+TEST_PATH = './../R/data_lstm_test.txt'
+train_data = loadtxt(TRAIN_PATH)
+test_data = loadtxt(TEST_PATH)
+print 'train_input_data shape: ',train_data.shape
+print 'test_input_data shape: ',test_data.shape
+
+#scaler  = MinMaxScaler( feature_range=(0, 1) )
+#train_data = scaler.fit_transform(train_data)
+#scaler  = MinMaxScaler( feature_range=(0, 1) )
+#test_data = scaler.fit_transform(test_data)
+
+X_train = []
+y_train = []
+X_test = []
+y_test = []
+X_train = train_data[:,0:length_of_sequences]
+y_train = train_data[:,length_of_sequences:length_of_sequences+in_out_neurons]
+X_test = test_data[:,0:length_of_sequences]
+y_test = test_data[:,length_of_sequences:length_of_sequences+in_out_neurons]
+
+X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
+X_test  = np.reshape(X_test,  (X_test.shape[0], X_test.shape[1], 1))
+
+print X_train.shape
+print y_train.shape
+print X_test.shape
+print y_test.shape
+
+from keras.models import Sequential
+from keras.layers.core import Dense, Activation, Dropout
+from keras.layers.recurrent import LSTM
+from keras.callbacks import EarlyStopping
+from keras.models import load_model
+
+#WHICH = 'TRAIN'
+WHICH = 'LOAD'
+batch_num = 10
+epoch = 50
+hidden_neurons = 300 # 中間層の数
+model = None
+
+if 'TRAIN' == WHICH:
+    model = Sequential()
+    model.add(LSTM(hidden_neurons, batch_input_shape=(None, length_of_sequences, 1), return_sequences=True))
+    #model.add(Dropout(0.5))
+    #model.add(Dense(hidden_neurons))
+    #model.add(Dropout(0.2))
+    model.add(LSTM(hidden_neurons/2, input_shape=(hidden_neurons/2, 1), return_sequences=True))
+    #model.add(Dropout(0.5))
+    #model.add(Dense(hidden_neurons/2))
+    #model.add(Dropout(0.2))
+    model.add(LSTM(hidden_neurons/2, input_shape=(hidden_neurons/2, 1), return_sequences=False))
+    #model.add(Dropout(0.5))
+    model.add(Dense(in_out_neurons))
+    #model.add(Dropout(0.2))
+    model.add(Activation("linear"))
+    model.compile(loss="mean_squared_error", optimizer="rmsprop")
+    early_stopping = EarlyStopping(monitor='val_loss', patience=0, verbose=0, mode='auto')
+    #model.fit(X_train, y_train, batch_size=batch_num, nb_epoch=epoch, validation_split=0.02, callbacks=[early_stopping])
+    model.fit(X_train, y_train, batch_size=batch_num, nb_epoch=epoch, validation_split=0.1)
+    model.summary()
+    model.save("model.h5")
+else:
+    model = load_model('model.h5')
+
+# X_testを入力に次の１つの要素を推測
+limit = 0.95
+anomaly_num = 50
+tn_count = 0
+fp_count = 0
+fn_count = 0
+tp_count = 0
+
+import scipy.spatial.distance
+predicted = model.predict(X_test)
+for index in range(0,predicted.shape[0]):
+    #print predicted[0]
+    #print y_test[0]
+    if index == anomaly_num:
+        print '***'
+    if index < anomaly_num:
+        if limit > 1 - scipy.spatial.distance.cosine(predicted[index],y_test[index]):
+            print 1 - scipy.spatial.distance.cosine(predicted[index],y_test[index]) , 'TN'
+            tn_count = tn_count + 1
+        else:
+            print 1 - scipy.spatial.distance.cosine(predicted[index],y_test[index]) , 'FN'
+            fn_count = fn_count + 1
+    else:
+        if limit <= 1 - scipy.spatial.distance.cosine(predicted[index],y_test[index]):
+            print 1 - scipy.spatial.distance.cosine(predicted[index],y_test[index]) , 'TP'
+            tp_count = tp_count + 1
+        else:
+            print 1 - scipy.spatial.distance.cosine(predicted[index],y_test[index]) , 'FP'
+            fp_count = fp_count + 1
+
+print 'TP:', tp_count
+print 'FP:', fp_count
+print 'TN:', tn_count
+print 'FN:', fn_count
+
+print 'accuracy: ', float(tp_count + tn_count)/float(tn_count + tp_count + fp_count + fn_count)
+print 'recall: ', float(tp_count)/float(tp_count + fn_count)
+print 'precision: ', float(tp_count)/float(tp_count + fp_count)
+print 'recall: ', float(tn_count)/float(tn_count + fp_count)
+print 'precision: ', float(tn_count)/float(tn_count + fn_count)
